e360d07ac54ee0710530a680f75c8c5bf67ef9cd
Dustin Speer <dspeer@outlook.com>
2023-02-03 22:39:35 -0500

    fix drops page

--- Pages/Participants/Drops.cshtml.cs
+++ Pages/Participants/Drops.cshtml.cs
@@ -8,17 +8,10 @@ namespace LOVIT.Tracker.Pages.Participants;
 public class DropsModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly IParticipantService _participantService;
-    private readonly ISegmentService _segmentService;
-    private readonly ICheckinService _checkinService;
     private readonly ILeaderService _leaderService;
 
-    public DropsModel(ILogger<IndexModel> logger, IParticipantService participantService, ISegmentService segmentService, ICheckinService checkinService, ILeaderService leaderService)
+    public DropsModel(ILogger<IndexModel> logger, ILeaderService leaderService)
     {
-        _logger = logger;
-        _participantService = participantService;
-        _segmentService = segmentService;
-        _checkinService = checkinService;
         _leaderService = leaderService;
     }
 
@@ -26,7 +19,6 @@ public class DropsModel : PageModel
 
     public async Task<IActionResult> OnGetAsync()
     {
-        var userId = User.Claims?.FirstOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", StringComparison.OrdinalIgnoreCase))?.Value;
         Leaders = await _leaderService.GetLeadersByDroppedStatusAsync();
 
         return Page();
